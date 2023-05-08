4553ba5ad7abb76fa99443af12855555ffbd809d
matt <matt@coders.co.uk>
2023-05-08 23:14:23 +0100

    Retest the phpcs check

--- Http/routes.php
+++ Http/routes.php
@@ -1,10 +1,12 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'unsubscriber', 'namespace' => 'Modules\UnSub\Http\Controllers'], function()
-{
+// Setup all of the routes :-)
+
+Route::group(['middleware' => 'web', 'prefix' => 'unsubscriber', 'namespace' => 'Modules\UnSub\Http\Controllers'], function () {
 
 	Route::get(
 		'/{mailbox_id}/{conversation_id}',
-		['uses' => 'UnSubController@index'])->name('unsub.index');
+		['uses' => 'UnSubController@index']
+	)->name('unsub.index');
 
 });
